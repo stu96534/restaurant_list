ac53175ee31b01784ee2e50be3aeed39ae96ac6c
stu96534 <stu96534@gmail.com>
2022-06-29 23:51:51 +0800

    feat: add delete restaurant function

--- app.js
+++ app.js
@@ -85,6 +85,15 @@ app.post('/restaurants/:restaurantId/edit', (req, res) => {
     .catch(error => console.error(error))
 })
 
+//setting remove restaurant
+app.post('/restaurants/:restaurantId/delete', (req, res) => {
+  const id = req.params.restaurantId
+  return Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect(`/`))
+    .catch(error => console.error(error))
+})
+
 //setting restaurants
 app.get('/restaurants/:restaurantId', (req, res) => {
   const restaurant = req.params.restaurantId
