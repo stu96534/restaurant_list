f083536b89316b351b9619e4499b1cdb790103ad
stu96534 <stu96534@gmail.com>
2022-07-03 23:59:05 +0800

    feat: add sort

--- app.js
+++ app.js
@@ -25,6 +25,8 @@ app.use(routes)
 
 
 
+
+
 //setting search
 
 
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const exphbs = require('express-handlebars')
 
 const Restaurant = require('../../models/restaurants')
 const restaurantList = require('../../models/seeds/restaurant.json')
 
+
 router.get('/', (req, res) => {
-  Restaurant.find({})
+  Restaurant.find()
     .lean()
     .then(restaurantList => res.render('index', { restaurantList }))
     .catch(error => console.error(error))
@@ -13,10 +15,35 @@ router.get('/', (req, res) => {
 
 router.get('/search', (req, res) => {
   const keyword = req.query.keyword
-  const restaurants = restaurantList.results.filter(list => {
-    return list.name.toLowerCase().includes(keyword.toLowerCase()) || list.category.toLowerCase().includes(keyword.toLowerCase())
-  })
-  res.render('index', { restaurantList: restaurants, keyword: keyword })
+  const sort = req.query.sort
+  let name = false
+  let name2 = false
+  let local = false
+  let cate = false
+
+  Restaurant.find({})
+    .lean()
+    .sort(sort)
+    .then(restaurantList => {
+      const restaurants = restaurantList.filter(list => {
+        return list.name.toLowerCase().includes(keyword.toLowerCase()) || list.category.toLowerCase().includes(keyword.toLowerCase())
+      })
+      if (sort === 'name') {
+        name = true
+      } else if (sort === '-name') {
+        name2 = true
+      } else if (sort === 'location') {
+        local = true
+      } else if (sort === 'category') {
+        cate = true
+      }
+
+      res.render('index', { restaurantList: restaurants, keyword, name, name2, local, cate })
+    })
+    .catch(error => console.error(error))
+  
 })
+
+
 
 module.exports = router
