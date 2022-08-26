d296be616350b8f42a335748cebdd3364a885e2e
stu96534 <stu96534@gmail.com>
2022-08-26 11:28:46 +0800

    feat: add login register pages

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const restaurant = require('./modules/restaurant')
+const User = require('./modules/users')
 
 router.use('/', home)
 router.use('/restaurants', restaurant)
+router.use('/users', User)
 
 module.exports = router
